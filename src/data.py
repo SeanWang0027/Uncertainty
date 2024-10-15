3df112c305413f274397d178c09556543f2a0bba
seanwang0027 <seanwang027@gmail.com>
2024-10-15 13:28:55 -0400

    [main] Sean updates the data selection.

---
+++ src/data.py
@@ -0,0 +1,38 @@
+"""Load Dataset."""
+import pickle
+from datasets import load_dataset
+
+
+def Load_SQuAD(path: str) -> None:
+    """Load the Dataset of SQuAD.
+
+    Args:
+        path (str): The path for storing the data in that pkl file.
+    """
+    # Load SQuAD dataset and tokenizer
+    dataset = load_dataset("squad")
+    train_dataset = dataset['train']
+    validation_dataset = dataset['validation']
+    train_store_path = f'{path}SQuAD_train.pkl'
+    validation_store_path = f'{path}SQuAD_validation.pkl'
+    for i in range(len(train_dataset)):
+        sample_dict = dict()
+        sample_dict['id'] = i
+        sample_dict['title'] = train_dataset[i]['title']
+        sample_dict['context'] = train_dataset[i]['context']
+        sample_dict['question'] = train_dataset[i]['question']
+        sample_dict['answer'] = train_dataset[i]['answers']['text'][0]
+        with open(train_store_path, 'ab') as f:
+            pickle.dump(sample_dict, f)
+    for i in range(len(validation_dataset)):
+        sample_dict = dict()
+        sample_dict['id'] = i
+        sample_dict['title'] = validation_dataset[i]['title']
+        sample_dict['context'] = validation_dataset[i]['context']
+        sample_dict['question'] = validation_dataset[i]['question']
+        sample_dict['answer'] = validation_dataset[i]['answers']['text'][0]
+        with open(validation_store_path, 'ab') as f:
+            pickle.dump(sample_dict, f)
+
+
+Load_SQuAD('../data/')
